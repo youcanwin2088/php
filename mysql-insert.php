c4a1ea4818adfbf69ed5639bf1a42fbf74381c33
wangzheng <wang@163.com>
2017-09-19 19:46:54 +0800

    a

--- mysql-insert.php
+++ mysql-insert.php
@@ -1,4 +1,19 @@
-﻿   
+﻿mysql库一般在用文本文件(source *.sql)或者控制台输入insert语句，来导入数据的时候，需要set names gbk,设置gbk会正确保存中文字符,并且在控制台正确显示中文，即使建设库的用的是utf8.（在文本文件里设置，控制台也一样
+ <br>SET NAMES gbk
+ <br>SET character_set_client = gbk */
+ <br>像ENGINE=InnoDB AUTO_INCREMENT=1016 DEFAULT CHARSET=utf8;这个不需要变
+ 
+ <br> 
+ 如果用的是set names latin1，虽然可以正确保存中文，但是保存的都是latin1格式，所以在用程序如php,读取时会比较麻烦，需要从utf8转换称gbk， 注意是utf8（用mb_convert_encoding函数），而不是latin1,而服务器端最终保存的都是utf8.<br>
+ 如果用set names utf8，反而无法正确保存中文,不要用utf8<br>
+ 
+ <br>在php语句里使用传统的mysql_connect后，在php5.2.*版本里，需要申明结果集用utf8格式   
+ mysql_query("SET character_set_results =utf8");
+ 而在php5.6版本里，则不需要；其它版本的php未测试 <br>
+ 
+ http://www.jb51.net/article/30864.htm<br>
+
+-----------------------------------    
 <form action="./mysql-insert.php" method="post">
 Firstname: <input type="text" name="firstname" />
 <input type="submit" value="插入这个表单数据"/>
@@ -14,7 +29,7 @@ Firstname: <input type="text" name="firstname" />
     $mysql_server_name="127.0.0.1"; //数据库服务器名称
     $mysql_username="root"; // 连接数据库用户名
     $mysql_password="111111"; // 连接数据库密码
-    $mysql_database="t1"; // 数据库的名字
+    $mysql_database="timesheet"; // 数据库的名字
     
     // 连接到数据库
     
@@ -25,7 +40,7 @@ Firstname: <input type="text" name="firstname" />
     $conn=mysql_connect($mysql_server_name, $mysql_username,
                         $mysql_password);
     
-   // mysql_query("SET character_set_result =gbk");
+    mysql_query("SET character_set_results =utf8");
                         
     //mysql_query("set names latin1");
     //mysql_set_charset('utf8', $conn); 
@@ -43,8 +58,8 @@ Firstname: <input type="text" name="firstname" />
     
                         
      // 从表中提取信息的sql语句
-    //$strsql="SELECT id,name,login FROM no_members limit 470,15";    
-    $strsql="SELECT name FROM s";
+    $strsql="SELECT id,name,login FROM no_members limit 470,15";    
+    //$strsql="SELECT name FROM s";
     // mysql_query("SET character_set_results = utf8");  
     // 执行sql查询
     
@@ -85,8 +100,8 @@ Firstname: <input type="text" name="firstname" />
         //echo mb_detect_encoding($name1);
         
         //echo mb_convert_encoding($name1,"UTF-8","UTF-8")." ";
-        //echo $row[$i];
-        echo mb_convert_encoding($row[$i],"UTF-8","gbk");
+        echo $row[$i];
+        //echo mb_convert_encoding($row[$i],"UTF-8","gbk");
         
         echo '</td>';
       }
